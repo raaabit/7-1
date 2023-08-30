bb2e6c933fa0dc621a5b34770f62d05997f31ccd
EC2 Default User <gsoejcbdus6@gmail.com>
2023-08-30 14:33:34 +0900

    8-4-1

--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,7 @@ use App\Http\Controllers\PostController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', function () { return view('welcome'); });
-Route::get('/posts', [PostController::class, 'index']);
+
+Route::get('/',[PostController::class, 'index']);
+Route::get('/posts/create', [PostController::class,'create']);
+Route::get('/posts/{post}', [PostController::class ,'show']);
